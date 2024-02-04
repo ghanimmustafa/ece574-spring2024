bed415729fbe3819b27f7ffffdcc24f8ce5cf1ab
Mustafa Ghanim <ghanim.mustafa@gmail.com>
2024-02-04 14:57:26 -0700

    adding a reg to z output of circuit 1, removing REG delays from estimated critical path calculations

--- homework1/circuits/474a_circuit1.v
+++ homework1/circuits/474a_circuit1.v
@@ -35,6 +35,7 @@ module circuit1(clk, rst, a,b,c,z,x);
     wire[15:0]f;
     wire[15:0]g;
     wire[15:0]xwire;
+    wire[7:0]zwire;
 
     wire gt;
     wire lt;
@@ -45,9 +46,10 @@ module circuit1(clk, rst, a,b,c,z,x);
     ADD #(.DATAWIDTH(8))ADD_2(a, c, e);
     COMP #(.DATAWIDTH(8))COMP_1(e, d, gt, lt, eq);
     assign g = {15'b0, gt} ;
-    MUX2x1 #(.DATAWIDTH(8))MUX2x1_1(d, e, g[0], z);
+    MUX2x1 #(.DATAWIDTH(8))MUX2x1_1(d, e, g[0], zwire);
     MUL #(.DATAWIDTH(16))MUL({8'b0, a}, {8'b0, c}, f);
     SUB #(.DATAWIDTH(16))SUB_1(f, {8'b0,d}, xwire);
     REG #(.DATAWIDTH(16))REG_1(clk, rst, xwire, x);
-    
+    REG #(.DATAWIDTH(8))REG_2(clk, rst, zwire, z);
+   
 endmodule
